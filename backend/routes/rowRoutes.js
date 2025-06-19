5389a47ac191c64dae9c03889c53f29fc9f1da62
Jesmin Mofi S <162531919+jesminmofi-cse@users.noreply.github.com>
2025-06-19 12:23:41 +0530

    Delete backend directory

--- backend/routes/rowRoutes.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {addRow}=require('../controllers/rowController');
-const authMiddleware=require('../middleware/authMiddleware');
-router.post('/:tableId/add-row',authMiddleware,addRow);
-module.exports=router;
--- backend/routes/tableRoutes.js
+++
@@ -1,6 +0,0 @@
-const express= require('express');
-const router =express.Router();
-const {createTable}=require('../controllers/tableController');
-const authMiddleware=require('../middleware/authMiddleware');
-router.post('/create', authMiddleware, createTable);
-module.exports=router;
