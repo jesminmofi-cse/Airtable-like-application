9d90e25bf9870ba2df93390bee3cb71d642d1e16
Jesmin Mofi S <162531919+jesminmofi-cse@users.noreply.github.com>
2025-06-25 09:10:30 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import TablePg from './pages/TablePg';
 import HistoryPage from './pages/HistoryPage';
 import EditTablePg from './pages/EditTablePg';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
-  const [token, setToken] = useState(null);
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    setToken(storedToken);
-  }, []);
-
   return (
     <Routes>
-      <Route path="/" element={token ? <Dashboard /> : <Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/table" element={<TablePg />} />
-      <Route path="/table/:id" element={<TablePg />} />
-      <Route path="/history" element={<HistoryPage />} />
-      <Route path="/edit/:id" element={<EditTablePg />} />
+      
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/table"
+        element={
+          <ProtectedRoute>
+            <TablePg />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/table/:id"
+        element={
+          <ProtectedRoute>
+            <TablePg />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/history"
+        element={
+          <ProtectedRoute>
+            <HistoryPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/edit/:id"
+        element={
+          <ProtectedRoute>
+            <EditTablePg />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
