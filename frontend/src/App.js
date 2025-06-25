849694d0ccfe3042be585bd78b971bc621a44e77
Jesmin Mofi S <162531919+jesminmofi-cse@users.noreply.github.com>
2025-06-25 09:00:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -6,8 +6,15 @@ import Dashboard from './pages/Dashboard';
 import TablePg from './pages/TablePg';
 import HistoryPage from './pages/HistoryPage';
 import EditTablePg from './pages/EditTablePg';
+
 const App = () => {
-  const token = localStorage.getItem('token');
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    setToken(storedToken);
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={token ? <Dashboard /> : <Navigate to="/login" />} />
@@ -15,9 +22,10 @@ const App = () => {
       <Route path="/register" element={<Register />} />
       <Route path="/table" element={<TablePg />} />
       <Route path="/table/:id" element={<TablePg />} />
-      <Route path='/history' element ={<HistoryPage/>}/>
-      <Route path='/edit/:id' element ={<EditTablePg/>}/>
+      <Route path="/history" element={<HistoryPage />} />
+      <Route path="/edit/:id" element={<EditTablePg />} />
     </Routes>
   );
 };
+
 export default App;
